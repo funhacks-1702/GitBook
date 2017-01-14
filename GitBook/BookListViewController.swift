7d26cc51d6d49d2a965d7b83e041a3f4ce851330
佐藤秀輔 <sato.fun0619@gmail.com>
2017-01-14 18:29:12 +0900

    [add] add BookListViewController

---
+++ GitBook/BookListViewController.swift
@@ -0,0 +1,36 @@
+//
+//  BookListViewController.swift
+//  GitBook
+//
+//  Created by 佐藤秀輔 on 2017/01/14.
+//  Copyright © 2017年 funhacks-1702. All rights reserved.
+//
+
+import UIKit
+import Kanna
+
+class BookListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
