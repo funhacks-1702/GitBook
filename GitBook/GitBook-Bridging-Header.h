fb2e6c3e71a6971069195e9b52191d7e4ca0f5e3
佐藤秀輔 <sato.fun0619@gmail.com>
2017-01-14 18:17:52 +0900

    [feature]Kannaの使用準備

---
+++ GitBook/GitBook-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  GitBook-Bridging-Header.h
+//  GitBook
+//
+//  Created by 佐藤秀輔 on 2017/01/14.
+//  Copyright © 2017年 funhacks-1702. All rights reserved.
+//
+
+#ifndef GitBook_Bridging_Header_h
+#define GitBook_Bridging_Header_h
+
+#import <libxml/HTMLtree.h>
+#import <libxml/xpath.h>
+#import <libxml/xpathInternals.h>
+
+#endif /* GitBook_Bridging_Header_h */
