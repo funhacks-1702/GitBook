fc4294fb6182596604fe590b382b2223a755d0ea
natmark <natmark0918@gmail.com>
2017-01-15 04:17:12 +0900

    [update] add assets & some controllers

---
+++ GitBook/WelcomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  WelcomeViewController.swift
+//  GitBook
+//
+//  Created by AtsuyaSato on 2017/01/15.
+//  Copyright © 2017年 funhacks-1702. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+/// ナビゲーションバー有効にする
+//    override func viewDidAppear(_ animated: Bool) {
+//        self.navigationController?.setNavigationBarHidden(false, animated: true)
+//    }
+//    override func viewWillDisappear(_ animated: Bool) {
+//        self.navigationController?.setNavigationBarHidden(true, animated: true)
+//    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
