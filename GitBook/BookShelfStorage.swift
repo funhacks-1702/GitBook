c86cfd24e1dfc065a0e561929563c3eeb218679d
jpfaw <isidoyuuta@gmail.com>
2017-01-14 23:24:41 +0900

    [add] Book Shelfを保存するRealmのモデルファイル

---
+++ GitBook/BookShelfStorage.swift
@@ -0,0 +1,17 @@
+//
+//  BookShelfStorage.swift
+//  GitBook
+//
+//  Created by Yuta on 2017/01/14.
+//  Copyright © 2017年 funhacks-1702. All rights reserved.
+//
+
+import RealmSwift
+
+class BookShelf: Object{
+    var shelf_id = 0
+    var shelf_name = ""
+    var shelf_owner = ""
+    var created_at = NSDate()
+    var shelf_books : [BookModel]?
+}
